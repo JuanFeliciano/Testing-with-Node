8bea97f0f691b9de28054a50e00ed146e4d93979
JuanFeliciano <139025070+JuanFeliciano@users.noreply.github.com>
2023-12-19 13:29:45 -0300

    created scheduling entity

---
+++ src/entities/appointment.ts
@@ -0,0 +1,25 @@
+export interface AppointmentProps {
+  customer: string
+  startsAt: Date
+  endsAt: Date
+}
+
+export class Appointment {
+  private props: AppointmentProps
+
+  get customer() {
+    return this.props.customer
+  }
+
+  get startsAt() {
+    return this.props.startsAt
+  }
+
+  get endsAt() {
+    return this.props.endsAt
+  }
+
+  constructor(props: AppointmentProps) {
+    this.props = props
+  }
+}
